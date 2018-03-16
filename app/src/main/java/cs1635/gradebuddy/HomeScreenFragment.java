0401df6a335de7510713e3762689a2d1dcf893a0
Jeremy Deppen <jmd221@pitt.edu>
2018-03-16 01:18:42 -0400

    Base project navigation

---
+++ app/src/main/java/cs1635/gradebuddy/HomeScreenFragment.java
@@ -0,0 +1,18 @@
+package cs1635.gradebuddy;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeScreenFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_screen, container, false);
+    }
+}
---
+++ app/src/main/java/cs1635/gradebuddy/MainActivity.java
@@ -0,0 +1,55 @@
+package cs1635.gradebuddy;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends FragmentActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    setFragment(new HomeScreenFragment());
+                    return true;
+                case R.id.navigation_calculate_gap:
+                    setFragment(new CalculateGpaFragment());
+                    return true;
+                case R.id.navigation_history:
+                    setFragment(new HistoryFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setFragment(new HomeScreenFragment());
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    protected void setFragment(Fragment fragment) {
+        android.support.v4.app.FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.contentFrame, fragment);
+        t.commit();
+    }
+
+}
